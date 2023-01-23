8c0b5cb9aa81f4dd200c32e76304a4d334648cc8
reXatrom <ajstylejoshua2837@gmail.com>
2023-01-23 03:11:35 -0800

    0x05. Python - Exceptions

---
+++ 0x05-python-exceptions/Python.h
@@ -0,0 +1,18 @@
+#ifndef PYTHON_H
+#define PYTHON_H
+
+def safe_print_list(my_list=[], x=0):
+def safe_print_integer(value):
+def safe_print_list_integers(my_list=[], x=0):
+def safe_print_division(a, b):
+def list_division(my_list_1, my_list_2, list_length):
+def raise_exception():
+def raise_exception_msg(message=""):
+def safe_print_integer_err(value):
+def safe_function(fct, *args):
+def magic_calculation(a, b):
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+
+#endif
