53481a7d5cd24f0a9c4d40712538931b23618143
reXatrom <ajstylejoshua2837@gmail.com>
2023-02-01 01:00:53 -0800

    correction

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,26 +1,33 @@
 #!/usr/bin/python3
-"""Matrix division"""
+"""
+Function that divides all element of a matrix.
+"""
 
 
 def matrix_divided(matrix, div):
-    """Divides every element of a matrix
-    Args:
-        matrix (list): A list of lists of integers
-        or floats div (int/float): The divisor.
     """
-
+    Divide a matrix number by number.
+    matrix: matrix to be divided.
+    div: number to divided the matrix.
+    Return: A list of list with the result.
+    """
+    newList = []
     if type(div) not in [int, float]:
         raise TypeError("div must be a number")
     if div == 0:
-        raise ZeroDivisionError(
-            "Each row of the matrix must have the same size")
-
-    if not all(len(row) == len(matrix[0]) for row in matrix):
-        raise TypeError("Each row of the matrix must have the same size")
-    if not all(type(num) in [int, float] for row in matrix for num in row):
-        raise TypeError("matrix must be a matrix (list of lists)"
-                        " of integers/floats")
-
-    new_matrix = [[eval("{:.2f}".format(num / div)) for num in row]
-                  for row in matrix]
-    return new_matrix
+        raise ZeroDivisionError("division by zero")
+    for i in matrix:
+        l = []
+        if type(i) is not list:
+            raise TypeError("matrix must be a matrix\
+ (list of lists) of integers/floats")
+        if(len(matrix[0]) != len(i)):
+            raise TypeError("Each row of the matrix\
+ must have the same size")
+        for j in range(len(i)):
+            if (type(i[j]) is not int) and (type(i[j]) is not float):
+                raise TypeError("matrix must be a matrix\
+(list of lists) of integers/floats")
+            l.append(round(((i[j]) / div), 2))
+        newList.append(l)
+    return newList
--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -7,8 +7,8 @@ def say_my_name(first_name, last_name=""):
     Validates string inputs, the display
     the full name
     """
-    if type(first_name) != str:
+    if type(first_name) is not str:
         raise TypeError("first name must be a string")
-    if type(last_name) != str:
+    if type(last_name) is not str:
         raise TypeError("last name must be a string")
-    print("My name is {} {}".format(first_name, last_name))
+    print("My name is", first_name, last_name)
