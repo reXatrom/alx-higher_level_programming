681cdf8183305b2573215e4ac878fd38e5e9ac53
reXatrom <ajstylejoshua2837@gmail.com>
2023-04-11 12:34:13 +0000

    correction

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 if (process.argv.length <= 3) {
   console.log('0');
-} 
-else {
-  const array = process.argv.slice(2).map(Number);
-  const sec = array.sort(function (a, b) { return b - a; })[1];
+} else {
+  const aray = process.argv.slice(2).map(Number);
+  const sec = aray.sort(function (a, b) { return b - a; })[1];
   console.log(sec);
 }
