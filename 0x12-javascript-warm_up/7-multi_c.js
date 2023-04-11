f16d46a2a77bdd39733b848075e3df6d1c8eb0e6
reXatrom <ajstylejoshua2837@gmail.com>
2023-04-11 12:41:42 +0000

    correction

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 
-let num = process.argv[2];
+const num = process.argv[2];
 
 if (!isNaN(parseInt(process.argv[2]))) {
   for (let a = 0; a < num; a++) {
     console.log('C is fun');
   }
-} 
-else {
+} else {
   console.log('Missing number of occurrences');
 }
