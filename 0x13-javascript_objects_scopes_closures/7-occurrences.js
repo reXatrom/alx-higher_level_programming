a59d3253d652344aa28798c88183076b0fdb5ec4
reXatrom <ajstylejoshua2837@gmail.com>
2023-04-12 22:37:03 +0000

    correction

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -3,7 +3,7 @@
 exports.nbOccurences = function (list, searchElement) {
   let counts = 0;
   for (let a = 0; a < list.length; a++) {
-    if (searchElement === list[a]) {
+    if (list[a] === searchElement) {
       counts++;
     }
   }
